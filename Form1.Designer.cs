c5c0448760b14ea202e545799f7818542c20b6dd
yuriangelica <yuriangelicasalazartrochez@gmail.com>
2023-10-04 22:37:21 -0500

    Formulario

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Formulario
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.lblCtrl = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblSemestre = new System.Windows.Forms.Label();
+            this.txtCtrl = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.cboSemestre = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image")));
+            this.picLogo.Location = new System.Drawing.Point(56, 37);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(125, 127);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 0;
+            this.picLogo.TabStop = false;
+            this.picLogo.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblCtrl
+            // 
+            this.lblCtrl.AutoSize = true;
+            this.lblCtrl.Location = new System.Drawing.Point(70, 220);
+            this.lblCtrl.Name = "lblCtrl";
+            this.lblCtrl.Size = new System.Drawing.Size(36, 13);
+            this.lblCtrl.TabIndex = 1;
+            this.lblCtrl.Text = "N. Ctrl";
+            this.lblCtrl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(70, 261);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblSemestre
+            // 
+            this.lblSemestre.AutoSize = true;
+            this.lblSemestre.Location = new System.Drawing.Point(70, 304);
+            this.lblSemestre.Name = "lblSemestre";
+            this.lblSemestre.Size = new System.Drawing.Size(51, 13);
+            this.lblSemestre.TabIndex = 3;
+            this.lblSemestre.Text = "Semestre";
+            this.lblSemestre.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // txtCtrl
+            // 
+            this.txtCtrl.Location = new System.Drawing.Point(139, 213);
+            this.txtCtrl.Name = "txtCtrl";
+            this.txtCtrl.Size = new System.Drawing.Size(100, 20);
+            this.txtCtrl.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(139, 254);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(200, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // cboSemestre
+            // 
+            this.cboSemestre.FormattingEnabled = true;
+            this.cboSemestre.Items.AddRange(new object[] {
+            "1 - Semestre",
+            "2 - Semestre",
+            "3 - Semestre",
+            "4 - Semestre",
+            "5 - Semestre",
+            "6 - Semestre",
+            "7 - Semestre",
+            "8 - Semestre",
+            "9 - Semestre"});
+            this.cboSemestre.Location = new System.Drawing.Point(139, 296);
+            this.cboSemestre.Name = "cboSemestre";
+            this.cboSemestre.Size = new System.Drawing.Size(121, 21);
+            this.cboSemestre.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cboSemestre);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCtrl);
+            this.Controls.Add(this.lblSemestre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblCtrl);
+            this.Controls.Add(this.picLogo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Label lblCtrl;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblSemestre;
+        private System.Windows.Forms.TextBox txtCtrl;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ComboBox cboSemestre;
+    }
+}
+
